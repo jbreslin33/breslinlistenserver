da73951e5b14666c545226e405dda6e3c6197612
jbreslin33 <jbreslin33@localhost>
2011-03-14 22:20:58 -0400

    moved key defines for now for linux

--- vclient/client.cpp
+++ vclient/client.cpp
@@ -7,13 +7,6 @@
 
 #include "common.h"
 
-#define VK_ESCAPE 0x1B
-#define VK_UP 0x26
-#define VK_DOWN 0x28
-#define VK_LEFT 0x25
-#define VK_RIGHT 0x27
-
-
 //-----------------------------------------------------------------------------
 // Name: empty()
 // Desc: 
--- vclient/common.h
+++ vclient/common.h
@@ -1,6 +1,11 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
+#define VK_ESCAPE 0x1B
+#define VK_UP 0x26
+#define VK_DOWN 0x28
+#define VK_LEFT 0x25
+#define VK_RIGHT 0x27
 
    #include "../tdreamsock/dreamSock.h"
 
